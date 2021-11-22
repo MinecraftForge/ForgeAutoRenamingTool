efda3508d705521459f0ceea1f7a4db72b170bde
zml <zml@stellardrift.ca>
2021-11-21 17:27:30 -0800

    Strip signature entries from remapped jars

--- src/main/java/net/minecraftforge/fart/internal/RenamingTransformer.java
+++ src/main/java/net/minecraftforge/fart/internal/RenamingTransformer.java
@@ -19,13 +19,20 @@
 
 package net.minecraftforge.fart.internal;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 import java.util.stream.Collectors;
 
 import org.objectweb.asm.ClassReader;
@@ -37,12 +44,15 @@ import net.minecraftforge.fart.api.Transformer;
 import net.minecraftforge.srgutils.IMappingFile;
 
 public class RenamingTransformer implements Transformer {
+    private static final Attributes.Name SHA_256_DIGEST = new Attributes.Name("SHA-256-Digest");
     private static final String ABSTRACT_FILE = "fernflower_abstract_parameter_names.txt";
     private final EnhancedRemapper remapper;
     private final Set<String> abstractParams = ConcurrentHashMap.newKeySet();
+    private final Consumer<String> log;
 
     public RenamingTransformer(Inheritance inh, IMappingFile map, Consumer<String> log) {
         this.remapper = new EnhancedRemapper(inh, map, log);
+        this.log = log;
     }
 
     @Override
@@ -63,6 +73,28 @@ public class RenamingTransformer implements Transformer {
 
     @Override
     public ManifestEntry process(ManifestEntry entry) {
+        // Remove all signature entries
+        try {
+            final Manifest manifest = new Manifest(new ByteArrayInputStream(entry.getData()));
+            boolean found = false;
+            for (final Iterator<Map.Entry<String, Attributes>> it = manifest.getEntries().entrySet().iterator(); it.hasNext();) {
+                final Map.Entry<String, Attributes> section = it.next();
+                if (section.getValue().remove(SHA_256_DIGEST) != null) {
+                    if (section.getValue().isEmpty()) {
+                        it.remove();
+                    }
+                    found = true;
+                }
+            }
+            if (found) {
+                try (final ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+                    manifest.write(os);
+                    return ManifestEntry.create(entry.getTime(), os.toByteArray());
+                }
+            }
+        } catch (final IOException ex) {
+            log.accept("Failed to remove signature entries from manifest: " + ex);
+        }
         return entry;
     }
 
@@ -70,6 +102,12 @@ public class RenamingTransformer implements Transformer {
     public ResourceEntry process(ResourceEntry entry) {
         if (ABSTRACT_FILE.equals(entry.getName()))
             return null;
+        if (entry.getName().startsWith("META-INF/")) {
+            if (entry.getName().endsWith(".RSA")
+                    || entry.getName().endsWith(".SF")) {
+                return null;
+            }
+        }
         return entry;
     }
 
