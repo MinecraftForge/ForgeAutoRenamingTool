67cda844238dae77a8576a667a0f526a544a053c
Paint_Ninja <PaintNinja@users.noreply.github.com>
2023-10-19 12:23:44 +0100

    Gradle 8.4, minor cleanup and optimisations

--- src/main/java/net/minecraftforge/fart/api/Renamer.java
+++ src/main/java/net/minecraftforge/fart/api/Renamer.java
@@ -2,7 +2,6 @@
  * Copyright (c) Forge Development LLC and contributors
  * SPDX-License-Identifier: LGPL-2.1-only
  */
-
 package net.minecraftforge.fart.api;
 
 import java.io.Closeable;
--- src/main/java/net/minecraftforge/fart/internal/ClassProviderBuilderImpl.java
+++ src/main/java/net/minecraftforge/fart/internal/ClassProviderBuilderImpl.java
@@ -2,7 +2,6 @@
  * Copyright (c) Forge Development LLC and contributors
  * SPDX-License-Identifier: LGPL-2.1-only
  */
-
 package net.minecraftforge.fart.internal;
 
 import java.io.IOException;
